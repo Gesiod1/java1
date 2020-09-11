4446ccc5e9ebb7d12b4e44981bc6c0d6c771f760
Gesiod1 <khorinvadim@gmail.com>
2020-09-11 19:32:27 +0300

    Hello World Homework 4.1

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -11,7 +11,27 @@ public class TriangleSimpleInfo {
             return c;
         }
     }
+    public static int minSide(int a, int b, int c){
+        if (a <= b && a <= c){
+            return a;
+        }
+        if ( b <= a && b <= c){
+            return  b;
+        } else {
+            return c;
+        }
+
+    }
+    public static boolean isEquilateralTriangle(int a, int b, int c){
+        if (a == b || a == c || b == c){
+            return true;
+        } else {
+            return  false;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println(maxSide(5, 6, 3));
+        System.out.println(maxSide(5, 3, 3));
+        System.out.println(minSide(5, 6, 8));
+        System.out.println(isEquilateralTriangle(6, 4, 3));
     }
 }
