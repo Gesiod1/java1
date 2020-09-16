b9e569cffe007519fe0562f11bd7f78a9dcfb91b
Gesiod1 <khorinvadim@gmail.com>
2020-09-16 15:58:35 +0300

    Hello World Homework 4.3

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,48 +1,56 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
+    final static double CONST_MIN = 1.61703;
+    final static double CONST_MAX = 1.61903;
     public static boolean containsDigit(int number, int digit){
         int remainderOfDivision; // остаток от деления на 10
+        if (number == digit && digit == 0){
+            return  true;
+        }
         while (number >= 1){
             remainderOfDivision = number % 10;
             if (remainderOfDivision == digit){
                 return true;
             }
             number/= 10;
+
         }
-        if (number == 0 && digit == 0) {
-                return true;
-            }
         return false;
     }
     public static int fiboNumber(int n){
-        int n0 = 0;
-        int i = 1;
-        while (i < n ){
-            i = n0 + (n0 = i);
+        int f0 = 0;
+        int f1 = 1;
+        int tmp;
+        for (int i = 0; i < n; i++) {
+            tmp = f1;
+            f1 = f1 + f0;
+            f0 = tmp;
+
         }
-        return n0;
+        return f1;
     }
     public static boolean isGoldenTriangle(int a, int b, int c) {
         /* создаем переменные для обозначения соотношения сторон,
          если треугольник равнобедренный */
-        double ratio1 = (double) a / c;    // если А = В
-        double ratio2 = (double) c / b;    // если А = С
-        double ratio3 = (double) b / a;    // если В = С
+        double ratio1 = ((double) a) / c;    // если А = В
+        double ratio2 = ((double) c) / b;    // если А = С
+        double ratio3 = ((double) b) / a;    // если В = С
         /* верхняя и нижняя границы */
-        double CONST_MIN = 1.61703;
-        double CONST_MAX = 1.61905;
-        if (a != b && b != c && a != c){ // условие, если треугольник не равнобедренный
-            return  false;
-        } else  { // проверяем, попадает ли соотношение сторон в наш диапазон
-            return  (CONST_MIN <= ratio1 && ratio1 <= CONST_MAX)||
-                    (CONST_MIN <= ratio2 && ratio2 <= CONST_MAX)||
+        if ((a == b && a > c) || (a == c && a > b) || (b == c && b > a)) {
+            // условие, если треугольник золотой
+            return (CONST_MIN <= ratio1 && ratio1 <= CONST_MAX) ||
+                    (CONST_MIN <= ratio2 && ratio2 <= CONST_MAX) ||
                     (CONST_MIN <= ratio3 && ratio3 <= CONST_MAX);
+        } else {
+            return false;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(fiboNumber(2));
+        System.out.print(isGoldenTriangle(55,89,89));
+        /* что требуется в 3.3 понимаю, но не знаю как реализовать
+         */
     }
 
 }
--- src/ru/progwards/java1/lessons/test/Test1.java
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -1,25 +1,45 @@
 package ru.progwards.java1.lessons.test;
 
 public class Test1 {
+    final static double CONST_MIN = 1.61703;
+    final static double CONST_MAX = 1.61903;
     public static boolean isGoldenTriangle(int a, int b, int c) {
         /* создаем переменные для обозначения соотношения сторон,
          если треугольник равнобедренный */
-        double ratio1 = (double) a / c;    // если А = В
-        double ratio2 = (double) c / b;    // если А = С
-        double ratio3 = (double) b / a;    // если В = С
+        double ratio1 = ((double) a) / c;    // если А = В
+        double ratio2 = ((double) c) / b;    // если А = С
+        double ratio3 = ((double) b) / a;    // если В = С
         /* верхняя и нижняя границы */
-        double CONST_MIN = 1.61703;
-        double CONST_MAX = 1.61905;
-        if (a != b && b != c && a != c){ // условие, если треугольник не равнобедренный
-            return  false;
-        } else  { // проверяем, попадает ли соотношение сторон в наш диапазон
-            return  (CONST_MIN <= ratio1 && ratio1 <= CONST_MAX)||
-                    (CONST_MIN <= ratio2 && ratio2 <= CONST_MAX)||
+        if ((a == b && a > c) || (a == c && a > b) || (b == c && b > a)) {
+            // условие, если треугольник золотой
+            return (CONST_MIN <= ratio1 && ratio1 <= CONST_MAX) ||
+                    (CONST_MIN <= ratio2 && ratio2 <= CONST_MAX) ||
                     (CONST_MIN <= ratio3 && ratio3 <= CONST_MAX);
+        } else {
+            return false;
         }
+    }
+    public static int fiboNumber(int n){
+        int n0 = 1;
+        int n1 = 1;
+        int n2 = 2;
+        if (n == n0){
+            n2 = n0;
+            return n2;
+        }
+        if (n == n1){
+            n2 = n1;
+            return  n2;
+        }
+        for (int i = 3; i <= n; i++ ){
+            n2 = n1 + n0;
+            n0 = n1;
+            n1 = n2;
+        }
+        return n2;
     }
 
     public static void main(String[] args) {
-        System.out.println(isGoldenTriangle(13, 21, 21));
+        System.out.println(isGoldenTriangle(55, 55, 89));
     }
 }
