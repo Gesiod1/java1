2ad67772a0ef7e4a31f1b01d264181b0fa10c8ab
Gesiod1 <khorinvadim@gmail.com>
2020-09-18 13:12:39 +0300

    H5

--- src/ru/progwards/java1/lessons/classes/Cow.java
+++ src/ru/progwards/java1/lessons/classes/Cow.java
@@ -1,6 +1,10 @@
 package ru.progwards.java1.lessons.classes;
 
 public class Cow extends Animal {
+    public Cow(double weight) {
+        super(weight);
+    }
+
     @Override
     public AnimalKind getKind(){
         return animal_сow;
--- src/ru/progwards/java1/lessons/classes/DUCK.java
+++ src/ru/progwards/java1/lessons/classes/DUCK.java
@@ -1,6 +1,10 @@
 package ru.progwards.java1.lessons.classes;
 
 public class DUCK extends  Animal{
+    public DUCK(double weight) {
+        super(weight);
+    }
+
     @Override
     public AnimalKind getKind(){
         return animal_duck;
