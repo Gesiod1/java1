cc7e6ed6f9c6469fc94a4484d9d54234f8114b1e
Gesiod1 <khorinvadim@gmail.com>
2020-09-18 19:10:31 +0300

    H5

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -20,12 +20,42 @@ public class ComplexNum {
         ComplexNum result = new ComplexNum(this.a + num.a, this.b + num.b);
         return result;
     }
-
-    public static void main(String[] args) {
-        ComplexNum z1 = new ComplexNum(34, 32);
-        ComplexNum z2 = new ComplexNum(14, 21);
-        ComplexNum result = z1.add(z2);
-        System.out.println(result);
+    // вычитание комплексных чисел по формуле:
+    // this - num = (this.a - num.a) + (this.b - num.b)i
+    public ComplexNum sub(ComplexNum num){
+        ComplexNum result = new ComplexNum(this.a - num.a, this.b - num.b);
+        return result;
+    }
+    // умножение комплексных чисел по формуле:
+    // this * num = (this.a * num.a - this.b * num.b) + (this.b * num.a + this.a * num.b)i
+    public ComplexNum mul(ComplexNum num){
+        ComplexNum result = new ComplexNum(this.a * num.a - this.b * num.b, this.b * num.a + this.a * num.b);
+        return result;
+    }
+    // деление комплексных чисел по формуле:
+    // this / num = (this.a * num.a + this.b * num.b) / (num.a * num.a + num.b * num.b) +
+    // ((this.b * num.a - this.b * num.b) / (num.a * num.a + num.b * num.b))i
+    public ComplexNum div(ComplexNum num){
+        ComplexNum result = new ComplexNum((this.a * num.a + this.b * num.b) / (num.a * num.a + num.b * num.b), (this.b * num.a - this.b * num.b) / (num.a * num.a + num.b * num.b));
+        return  result;
     }
 
+    public static void main(String[] args) {
+        ComplexNum z1 = new ComplexNum(5, 4);
+        ComplexNum z2 = new ComplexNum(8, 2);
+        ComplexNum z3 = new ComplexNum(5, 4);
+        ComplexNum z4 = new ComplexNum(8, 2);
+        ComplexNum z5 = new ComplexNum(5, 4);
+        ComplexNum z6 = new ComplexNum(8, 2);
+        ComplexNum z7 = new ComplexNum(5, 4);
+        ComplexNum z8 = new ComplexNum(8, 2);
+        ComplexNum result_add = z1.add(z2);
+        ComplexNum result_sub = z3.sub(z4);
+        ComplexNum result_mul = z5.mul(z6);
+        ComplexNum result_div = z7.div(z8);
+        System.out.println(result_add);
+        System.out.println(result_sub);
+        System.out.println(result_mul);
+        System.out.println(result_div);
+    }
 }
