8a4943b59a59bb0e3555401fa34d75a4e9ca39c1
Gesiod1 <khorinvadim@gmail.com>
2020-10-08 15:06:58 +0300

    H6

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -11,9 +11,10 @@ public class Binary {
     @Override
     public String toString() {
         String tmp = "";
-        while (num > 0){
-            int bit = num & 1; // остаток от деления числа num
-            num >>= 1; // число для следующей итерации
+        int value = num & 0b1111_1111;
+        for (int i = 0; i < 8; i++){
+            int bit = value & 1; // остаток от деления числа num
+            value >>= 1; // число для следующей итерации
             String bitStr = Integer.toString(bit);
             tmp = bitStr + tmp; // добавляем новое значение bit вперед
         }
@@ -21,7 +22,7 @@ public class Binary {
     }
 
     public static void main(String[] args) {
-        Binary firstNum = new Binary((byte) 127);
+        Binary firstNum = new Binary((byte) -128);
         System.out.println(firstNum);
     }
 }
