dd1edceaa91b269079b5199f94a21e43b7702bb6
Gesiod1 <khorinvadim@gmail.com>
2020-10-08 14:40:04 +0300

    H6

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -1,12 +1,27 @@
 package ru.progwards.java1.lessons.bitsworld;
 
 public class Binary {
-    public Binary(byte num){}
+    byte num;
+
+    public Binary(byte num){
+        this.num = num;
+    }
 
     // возвращает двоичное представление числа типа byte, используя только битовые операции
-//    public String toString(){}
-//
-//    public static void main(String[] args) {
-//
-//    }
+    @Override
+    public String toString() {
+        String tmp = "";
+        while (num > 0){
+            int bit = num & 1; // остаток от деления числа num
+            num >>= 1; // число для следующей итерации
+            String bitStr = Integer.toString(bit);
+            tmp = bitStr + tmp; // добавляем новое значение bit вперед
+        }
+        return tmp;
+    }
+
+    public static void main(String[] args) {
+        Binary firstNum = new Binary((byte) 127);
+        System.out.println(firstNum);
+    }
 }
