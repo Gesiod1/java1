23cf9cf54f50bd89604d6d371cf2f8aa45ab08f9
Gesiod1 <khorinvadim@gmail.com>
2020-11-12 11:55:11 +0300

    H9

---
+++ src/ru/progwards/java1/lessons/io1/CharFilter.java
@@ -0,0 +1,38 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CharFilter {
+    public static void filterFile(String inFileName, String outFileName, String filter) throws FileNotFoundException {
+        try {
+            FileReader reader = new FileReader(inFileName);
+            Scanner scanner = new Scanner(reader);
+            char[] filterToChar = filter.toCharArray();
+            while (scanner.hasNextLine()){
+                String strNextLine = scanner.nextLine();
+                char[] nextLineToChar = strNextLine.toCharArray();
+                for (int i = 0; i < nextLineToChar.length; i++) {
+                    for (int j = 0; j < filterToChar.length; j++) {
+                        if (nextLineToChar[i] != filterToChar[j]){
+                            FileWriter writer = new FileWriter(outFileName);
+                            writer.write(nextLineToChar[i]);
+                            writer.close();
+                        }
+                    }
+
+                }
+            }
+        }catch (FileNotFoundException e){} catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String filter1 = "be";
+        filterFile("forRead", "forWrite", filter1);
+    }
+}
