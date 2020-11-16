8f055ce3f0f04116074570e7e6fcfbf604bd29b2
Gesiod1 <khorinvadim@gmail.com>
2020-11-16 23:59:45 +0300

    H10.2

---
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -0,0 +1,47 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Coder {
+    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) throws IOException {
+        try{
+            FileReader reader = new FileReader(inFileName);
+            Scanner scanner = new Scanner(reader);
+            FileWriter writer = new FileWriter(outFileName);
+            while (scanner.hasNextLine()){
+                String nextLine = scanner.nextLine();
+                String charNextLine = new String();
+                byte[] arrayNextLine = new byte [nextLine.length()];
+                for (int i = 0; i < nextLine.length(); i++) {
+                    arrayNextLine[i] = (byte) nextLine.charAt(i);
+                }
+                for (int i = 0; i < nextLine.length(); i++) {
+                    arrayNextLine[i] = (byte) code[arrayNextLine[i]];
+                    charNextLine += (char) arrayNextLine[i];
+                }
+                writer.write(charNextLine + "\n");
+            }
+            writer.close();
+        } catch (FileNotFoundException e){
+            FileWriter logWrite = new FileWriter(logName);
+            logWrite.write(e.getMessage());
+            logWrite.close();
+            throw new IOException(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        char[] test1 = new char[255];
+        for (int i = 254; i > 0; i--) {
+            test1[i] = (char) (Math.random()*127)
+            ;
+        }
+
+        codeFile("forRead.txt", "forWrite.txt", test1, "logfile.txt");
+    }
+}
