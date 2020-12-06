07baf4e3e341235fb205f057ae91677bb0b7fe69
Gesiod1 <khorinvadim@gmail.com>
2020-12-06 22:03:57 +0300

    Censor

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -15,7 +15,7 @@ public class Censor {
          }
          @Override
          public String toString(){
-             return filename + ": " + message;
+             return filename + ":" + message;
          }
 
      }
