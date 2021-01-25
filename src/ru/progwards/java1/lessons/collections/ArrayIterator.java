0b16de2ba0fc5eb426e7b98fa889ed9e67a615f1
Gesiod1 <khorinvadim@gmail.com>
2021-01-25 17:07:05 +0300

    12 collections

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -2,9 +2,12 @@ package ru.progwards.java1.lessons.collections;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+//Сделать итератор по одномерному массиву, реализовать методы hasNext() и next()
 public class ArrayIterator<T> implements Iterator<T>  {
     private T[] array;
+    private  Integer index = 0;
 
     ArrayIterator(T[] array) {
         this.array = array;
@@ -12,12 +15,24 @@ public class ArrayIterator<T> implements Iterator<T>  {
 
     @Override
     public boolean hasNext() {
-         return Arrays.stream(this.array).iterator().hasNext();
+         if (this.array.length == 0){
+             return true;
+         }
+         return false;
     }
 
     @Override
-    public T next() {
-        return Arrays.stream(this.array).iterator().next();
+    public T next() throws NoSuchElementException{
+        if (array.length <=0){
+            throw new NoSuchElementException("No more elements in array!");
+        }
+        if (index == 0){
+            return array[index++];
+        }
+        if (index + 1 == array.length - 1){
+            return array[++index];
+        }
+        return  index + 1 < array.length + 1 ? array[++index]: null;
     }
 
 }
