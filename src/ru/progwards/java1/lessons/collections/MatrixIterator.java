737aaab269991583364776b0475d242a6d852edd
Gesiod1 <khorinvadim@gmail.com>
2021-01-28 19:46:25 +0300

    12 collections

--- src/ru/progwards/java1/lessons/collections/MatrixIterator.java
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -2,21 +2,50 @@ package ru.progwards.java1.lessons.collections;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class MatrixIterator<T> implements Iterator<T> {
-    private T[] array;
+    private T[][] array;
+    private Integer index = 0;
+    private Integer indexNested = 0;
+    private Integer nestedLenght = 0;
 
-    MatrixIterator(T[] array) {
-        this.array = array;
+    MatrixIterator(T[][] array) {
+        this.array =  array;
+        updateNextArrayLengt();
+    }
+    private void updateNextArrayLengt(){
+        if (this.nestedLenght == 0 && index < this.array.length){
+            this.nestedLenght = this.array[index].length;
+        }
     }
 
     @Override
     public boolean hasNext() {
-        return Arrays.stream(this.array).iterator().hasNext();
+        if (array.length == 0 || (index == array.length - 1 && indexNested == array[index].length)){
+            return false;
+        }
+        return index != this.array.length;
     }
 
     @Override
-    public T next() {
-        return Arrays.stream(this.array).iterator().next();
+    public T next() throws NoSuchElementException{
+        if (array.length <= 0){
+            throw new NoSuchElementException("No more elements in array!");
+        }
+        if (indexNested.equals(nestedLenght)){
+            index++;
+            indexNested = nestedLenght = 0;
+        }
+        updateNextArrayLengt();
+        return  index < array.length ? array[index][indexNested++] : null;
+    }
+
+    public static void main(String[] args) {
+        String [][] test = {{"a", "b"}, {"d", "d", "c"}, {"a", "z"}};
+        MatrixIterator<String> matrixIterator = new MatrixIterator<String>(test);
+        while (matrixIterator.hasNext()){
+            System.out.println(matrixIterator.next());
+        }
     }
 }
