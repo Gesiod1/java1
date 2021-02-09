6af3acc1b276dd633d6c00466f0a9750edee267c
Gesiod1 <khorinvadim@gmail.com>
2021-02-09 17:07:51 +0300

    13 sets

---
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -0,0 +1,31 @@
+package ru.progwards.java1.lessons.sets;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class LettersInFile {
+
+
+    //вернуть все буквы, которые встретились в файле, сконкатенированные в виде строки.
+    // Буквы должны быть упорядочены по алфавиту, типа “ADEF...”.
+    // Все возникающие исключения, по работе с потоками, пробросить выше
+    public static String process(String fileName){
+        Set<String> result = new TreeSet<>();
+        StringBuilder resultOnString = new StringBuilder();
+        char [] wordSymbols = fileName.toCharArray();
+        for (int i = 0; i < wordSymbols.length; i++) {
+            if (Character.isAlphabetic(wordSymbols[i])) {
+                result.add(String.valueOf(wordSymbols[i]));
+            }
+        }
+        for (String str: result) {
+            resultOnString.append(str);
+        }
+        return String.valueOf(resultOnString);
+    }
+
+    public static void main(String[] args) {
+        LettersInFile test = new LettersInFile();
+        System.out.println(test.process("12A!@ZZXS43cd"));
+    }
+}
