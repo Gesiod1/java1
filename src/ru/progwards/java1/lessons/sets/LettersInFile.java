f21251f8a81338de62a22b6f04f0bae31ed52f34
Gesiod1 <khorinvadim@gmail.com>
2021-02-10 12:30:29 +0300

    13 sets

--- src/ru/progwards/java1/lessons/sets/LettersInFile.java
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -1,5 +1,8 @@
 package ru.progwards.java1.lessons.sets;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -9,16 +12,19 @@ public class LettersInFile {
     //вернуть все буквы, которые встретились в файле, сконкатенированные в виде строки.
     // Буквы должны быть упорядочены по алфавиту, типа “ADEF...”.
     // Все возникающие исключения, по работе с потоками, пробросить выше
-    public static String process(String fileName){
+    public static String process(String fileName) throws FileNotFoundException {
+        FileReader fileReader = new FileReader(fileName);
+        Scanner scanner = new Scanner(fileReader);
+
         Set<String> result = new TreeSet<>();
         StringBuilder resultOnString = new StringBuilder();
-        char [] wordSymbols = fileName.toCharArray();
-        for (int i = 0; i < wordSymbols.length; i++) {
-            if (Character.isAlphabetic(wordSymbols[i]) && Character.isUpperCase(wordSymbols[i])) {
-                result.add(String.valueOf(wordSymbols[i]).toUpperCase());
-            }
-            if (Character.isAlphabetic(wordSymbols[i]) && Character.isLowerCase(wordSymbols[i])) {
-                result.add(String.valueOf(wordSymbols[i]).toLowerCase());
+
+        while (scanner.hasNextLine()){
+            char [] wordSymbols = scanner.nextLine().toCharArray();
+            for (int i = 0; i < wordSymbols.length; i++) {
+                if (Character.isAlphabetic(wordSymbols[i]) ) {
+                    result.add(String.valueOf(wordSymbols[i]));
+                }
             }
         }
         for (String str: result) {
@@ -27,8 +33,8 @@ public class LettersInFile {
         return String.valueOf(resultOnString);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         LettersInFile test = new LettersInFile();
-        System.out.println(test.process("12A!@ZZXS43cd"));
+        System.out.println(process("forRead.txt"));
     }
 }
