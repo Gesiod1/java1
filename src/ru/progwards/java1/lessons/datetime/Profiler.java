1174df92115276e55155666fa36528795d8c7634
Gesiod1 <khorinvadim@gmail.com>
2021-05-18 16:29:50 +0300

    16 time

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -1,83 +1,171 @@
 package ru.progwards.java1.lessons.datetime;
 
-import java.time.Instant;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.*;
 
-public class Profiler {
-    public static List<StatisticInfo> statisticInfo = new ArrayList<>();
+public class Profiler
+{
+    static TreeMap<String, StatisticInfo> tree = new TreeMap<>();
 
-    public static void enterSection(String name) {
-        Instant instant = Instant.now();
+    static List<StackItem> stack = new ArrayList<>();
 
-        for (int i = 0; i < statisticInfo.size(); i++) {
-            StatisticInfo current = statisticInfo.get(i);
-            if (current.sectionName.equals(name)) {
-                current.selfTime = instant.toEpochMilli();
-                statisticInfo.set(i, current);
-                return;
-            }
+    public static void enterSection(String name)
+    //- войти в профилировочную секцию, замерить время входа.
+    {
+        stack.add(new StackItem());
+    }
+
+    public static void exitSection(String name)
+    //- выйти из профилировочной секции.
+//        Замерить время выхода, вычислить промежуток времени между входом и выходом в миллисекундах.
+    {
+//убрать послений из стека
+        StackItem si = stack.get(stack.size() - 1);
+        si.calcTimes();
+        stack.remove(si);
+
+//вычесть из всех  остальных в стеке
+        for (int i = stack.size() - 1; i >= 0; i--)
+        {
+            StackItem cur = stack.get(i);
+            cur.selfTime = cur.selfTime - si.selfTime;
         }
 
-        StatisticInfo stInfo = new StatisticInfo();
-        stInfo.sectionName = name;
-        stInfo.selfTime = instant.toEpochMilli();;
-        statisticInfo.add(stInfo);
-    }
-
-    public static void exitSection(String name) {
-        Instant instant = Instant.now();
-
-        for (int i = 0; i < statisticInfo.size(); i++) {
-            StatisticInfo current = statisticInfo.get(i);
-            if (current.sectionName.equals(name)) {
-                current.selfTime = instant.toEpochMilli() - current.selfTime;
-                current.count++;
-                current.fullTime = current.count > 1 ? current.fullTime + current.selfTime : 0;
-                statisticInfo.set(i, current);
-                break;
-            }
+//добавить в список
+        StatisticInfo statisticInfo = tree.get(name);
+        if (statisticInfo == null)
+        {
+            tree.put(name, StatisticInfo.update(name, si));
+        } else
+        {
+            statisticInfo.fullTime = statisticInfo.fullTime + si.fullTime;
+            statisticInfo.selfTime = statisticInfo.selfTime + si.selfTime;
+            statisticInfo.count++;
         }
     }
 
-    public static List<StatisticInfo> getStatisticInfo() {
-
-        return statisticInfo;
+    public static List<StatisticInfo> getStatisticInfo()
+    //получить профилировочную статистику, отсортировать по наименованию секции
+    {
+        List<StatisticInfo> list = new ArrayList<>();
+        for (var entry : tree.entrySet())
+        {
+            list.add(entry.getValue());
+        }
+        return list;
     }
 
-    public static void main(String[] args) {
-        long val = 0;
+    public static void main(String[] args) throws InterruptedException
+    {
         enterSection("1");
-        for (int i = 0; i< 1000000; i++) {
-            enterSection("2");
-            val += i * 2;
-            System.out.println(val + '\n');
-            exitSection("2");
-        }
-        for (int i = 0; i< 10; i++) {
+        Thread.sleep(10);
+
+        for (int i = 0; i < 5; i++)
+        {
+
             enterSection("3");
-            val += i * 2;
-            System.out.println(val + '\n');
+            Thread.sleep(30);
+
+            enterSection("5");
+            Thread.sleep(50);
+            exitSection("5");
+
             exitSection("3");
+
         }
-        exitSection("1");
-        System.out.println(getStatisticInfo().get(0).sectionName + ": " + getStatisticInfo().get(0).selfTime);
-        System.out.println(getStatisticInfo().get(1).sectionName + ": " + getStatisticInfo().get(1).selfTime);
-        System.out.println(getStatisticInfo().get(1).sectionName + ": " + getStatisticInfo().get(1).fullTime);
-        System.out.println(getStatisticInfo().get(2).sectionName + ": " + getStatisticInfo().get(2).selfTime);
-        System.out.println(getStatisticInfo().get(2).sectionName + ": " + getStatisticInfo().get(2).fullTime);
+
         enterSection("4");
-        for (int i = 0; i< 10000; i++) {
-            val += i * 2;
-            System.out.println(val + '\n');
-        }
+        Thread.sleep(40);
         exitSection("4");
-        System.out.println(getStatisticInfo().get(3).sectionName + ": " + getStatisticInfo().get(3).selfTime);
-        System.out.println(getStatisticInfo().get(3).sectionName + ": " + getStatisticInfo().get(3).fullTime);
+
+        exitSection("1");
+
+        System.out.println(getStatisticInfo());
     }
 }
-    class StatisticInfo {
-        public String sectionName;
-        public long fullTime = 0;
-        public long selfTime = 0;
-        public int count = 0;
+
+class StackItem
+{
+    public int fullTime;// полное время выполнения секции в миллисекундах.
+    public int selfTime;// чистое время выполнения секции в миллисекундах.
+    public LocalDateTime startTime;
+
+    StackItem()
+    {
+        selfTime = 0;
+        startTime = LocalDateTime.now();
     }
+
+    void calcTimes()
+    {
+        fullTime = (int) Duration.between(startTime, LocalDateTime.now()).toMillis();
+        selfTime = selfTime + fullTime;
+    }
+}
+
+class StatisticInfo
+{
+    public String sectionName; // имя секции
+    public int fullTime;// полное время выполнения секции в миллисекундах.
+    public int selfTime;// чистое время выполнения секции в миллисекундах.
+    // Для вложенных секций, из времени выполнения внешней секции нужно вычесть времена
+// выполнения вложенных секций.
+    public int count;// количество вызовов. В случае, если вызовов более одного, fullTime и selfTime
+// содержат суммарное время выполнения всех вызовов.
+
+    static StatisticInfo update(String name, StackItem item)
+    {
+        StatisticInfo res = new StatisticInfo();
+        res.sectionName = name;
+        res.fullTime = item.fullTime;
+        res.selfTime = item.selfTime;
+        res.count = 1;
+        return res;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "StatisticInfo{" +
+                "sectionName='" + sectionName + '\'' +
+                ", fullTime=" + fullTime +
+                ", selfTime=" + selfTime +
+                ", count=" + count +
+                '}' + "\n";
+    }
+
+//    public static void main(String[] args) {
+//        long val = 0;
+//        enterSection("1");
+//        for (int i = 0; i< 1000000; i++) {
+//            enterSection("2");
+//            val += i * 2;
+//            System.out.println(val + '\n');
+//            exitSection("2");
+//        }
+//        for (int i = 0; i< 10; i++) {
+//            enterSection("3");
+//            val += i * 2;
+//            System.out.println(val + '\n');
+//            exitSection("3");
+//        }
+//        exitSection("1");
+//        System.out.println(getStatisticInfo().get(0).sectionName + ": " + getStatisticInfo().get(0).selfTime);
+//        System.out.println(getStatisticInfo().get(1).sectionName + ": " + getStatisticInfo().get(1).selfTime);
+//        System.out.println(getStatisticInfo().get(1).sectionName + ": " + getStatisticInfo().get(1).fullTime);
+//        System.out.println(getStatisticInfo().get(2).sectionName + ": " + getStatisticInfo().get(2).selfTime);
+//        System.out.println(getStatisticInfo().get(2).sectionName + ": " + getStatisticInfo().get(2).fullTime);
+//        enterSection("4");
+//        for (int i = 0; i< 10000; i++) {
+//            val += i * 2;
+//            System.out.println(val + '\n');
+//        }
+//        exitSection("4");
+//        System.out.println(getStatisticInfo().get(3).sectionName + ": " + getStatisticInfo().get(3).selfTime);
+//        System.out.println(getStatisticInfo().get(3).sectionName + ": " + getStatisticInfo().get(3).fullTime);
+//    }
+}
+
+
+
