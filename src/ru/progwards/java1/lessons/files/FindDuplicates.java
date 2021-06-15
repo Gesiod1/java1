243689bfc04d72e4dad7dffa5fb0c57346d6b90e
Gesiod1 <khorinvadim@gmail.com>
2021-06-15 15:28:19 +0300

    17 nio

--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -11,7 +11,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class FindDuplicates {
-    List<Path> allFiles = new ArrayList<>(); // список с путями всех файлов с текущего каталога
+    List<Path> allFiles = new LinkedList<>(); // список с путями всех файлов с текущего каталога
 
     public List<List<String>> findDuplicates(String startPath) throws IOException {
         PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**");
@@ -43,22 +43,24 @@ public class FindDuplicates {
         List<List<String>> allDuplicates = new ArrayList<>();
         while (pathList.size() > 1){
             List<String> fileInfo = new ArrayList<>(); // список для записи одинаковых файлов
+            fileInfo.clear();
             for (int i = 1; i < pathList.size(); i++) {
                 if (fileInfo.size() == 0){ // добавляем файл из начала pathList и сравниваем с ним другие из остального списка
                     fileInfo.add(String.valueOf(pathList.get(0).getFileName()) + " " + pathList.get(0).toAbsolutePath());
                 }
-                if (pathList.get(0).getFileName().equals(pathList.get(i)) &&
+                if (pathList.get(0).getFileName().equals(pathList.get(i).getFileName()) &&
                         fileExpansion(pathList.get(0)).equals(fileExpansion(pathList.get(i))) &&
                         Files.getLastModifiedTime(pathList.get(0)).equals(Files.getLastModifiedTime(pathList.get(i))) &&
                         Files.size(pathList.get(0)) == Files.size(pathList.get(i)) &&
                         Files.readString(pathList.get(0)).equals(Files.readString(pathList.get(i)))){
                     // если дубликаты есть, то добавляем в fileInfo имя файла и полный путь
                     fileInfo.add(String.valueOf(pathList.get(i).getFileName()) + " " + pathList.get(i).toAbsolutePath());
+                    pathList.remove(i);
+                    i--;
                 }
             }
             allDuplicates.add(fileInfo);
             pathList.remove(0);
-            fileInfo.clear();
         }
         // удаляем из списка списков все файлы, у которых нет дубликатов
         for (int i = 0; i < allDuplicates.size(); i++) {
@@ -90,11 +92,11 @@ public class FindDuplicates {
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 if (pathMatcher.matches(file))
                     Files.setLastModifiedTime(file, Files.getLastModifiedTime(Paths.get(HOME_DIR + "path1/path2/path3/file1.txt")));
-                    System.out.println(file.getFileName());
-                    System.out.println(Files.getLastModifiedTime(file));
-                    System.out.println(Files.getAttribute(file, "size"));
-                    System.out.println(fileExpansion(file));
-                    System.out.println();
+//                    System.out.println(file.getFileName());
+//                    System.out.println(Files.getLastModifiedTime(file));
+//                    System.out.println(Files.getAttribute(file, "size"));
+//                    System.out.println(fileExpansion(file));
+//                    System.out.println();
                 return FileVisitResult.CONTINUE;
             }
 
@@ -104,8 +106,7 @@ public class FindDuplicates {
             }
         });
         System.out.println(findDuplicates.findDuplicates(HOME_DIR));
-        System.out.println(findDuplicates.allFiles.size());
-
+//        System.out.println(findDuplicates.allFiles.size());
     }
 }
 
